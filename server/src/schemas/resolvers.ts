384d756e97f626430743f4221690ad64e622108b
BodyKount <cness1989@icloud.com>
2025-04-07 16:11:03 -0700

    upload

---
+++ server/src/schemas/resolvers.ts
@@ -0,0 +1,62 @@
+import { User } from '../models/index.js';
+import { signToken, AuthenticationError } from '../services/auth.js';
+
+const resolvers = {
+  Query: {
+    me: async (_parent: any, _args: any, context: any): Promise<any | null> => {
+      
+      if (context.user) {
+
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+        return userData;
+      }
+      throw new AuthenticationError('User not authenticated');
+    },
+  },
+  Mutation: {
+    addUser: async (_parent: any, args: any): Promise<{ token: string; user: any }> => {
+      const user = await User.create(args);
+      const token = signToken(user.username, user.email, user._id);
+            
+      return { token, user };
+    },
+    login: async (_parent: any, { email, password }: { email: string; password: string }): Promise<{ token: string; user: any }> => {
+      const user = await User.findOne({ email });
+
+      if (!user || !(await user.isCorrectPassword(password))) {
+        throw new AuthenticationError('Invalid credentials');
+      }
+
+      const token = signToken(user.username, user.email, user._id);
+      return { token, user };
+    },
+    saveBook: async (_parent: any, { bookData }: { bookData: any }, context: any): Promise<any | null> => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: bookData } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('User not authenticated');
+    },
+    removeBook: async (_parent: any, { bookId }: { bookId: string }, context: any): Promise<any | null> => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('User not authenticated');
+    },
+  },
+};
+
+export default resolvers;
